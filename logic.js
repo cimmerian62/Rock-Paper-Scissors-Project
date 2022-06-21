e3f68545ce0a6b63f82450c9385e1e4ba02cc983
Josiah Luikham <cimmerian62@gmail.com>
2022-06-21 14:45:40 -0400

    Finish rps-ui branch

--- logic.js
+++ logic.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let compScore = 0;
+
 function computerPlay() {
     let decider = Math.random();
     if (decider <= 0.333) {
@@ -13,81 +16,83 @@ function computerPlay() {
         return "scissors";
     }
 }
+function displayRes(playerSelection, computerSelection) {
+    pScore.textContent = `Player: ${playerScore}`;
+    cScore.textContent = `Computer: ${compScore}`;
+    chose.textContent = `Player chose ${playerSelection}, Computer chose ${computerSelection}`;
+    if (playerScore >= 5) {
+        res.textContent = "Player wins!"
+    }
+    else if (compScore >= 5) {
+        res.textContent = "Computer wins!"
+    }
+}
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection == 'rock') {
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    if (playerScore >= 5 || compScore >= 5) {
+        playerScore = 0;
+        compScore = 0;
+        res.textContent = "";
+    }
+    if (playerSelection === 'rock') {
         switch(computerSelection) {
-            case 'rock':
-                return 0;
-            case 'scissors':
-                return 1;
-            default:
-                return 2;
-        }
-    }
-    if (playerSelection == 'paper') {
-        switch(computerSelection) {
-            case 'rock':
-                return 1;
-            case 'scissors':
-                return 2;
-            default:
-                return 0;
-        }
-    }
-    if (playerSelection == 'scissors') {
-        switch(computerSelection) {
-            case 'rock':
-                return 2;
-            case 'scissors':
-                return 0;
-            default:
-                return 1;
-        }
-    }
-}
-function getInput() {
-    while(true) {
-        let res = prompt("Do you choose Rock, Paper, or Scissors?")
-        res.toLowerCase();
-        if (res == 'rock' || res == 'paper' || res == 'scissors') {
-            return res;
-        }
-        else {
-            console.log('Please enter a valid input.');
-        }
-    }
-}
-function game() {
-    let compScore = 0;
-    let playerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let compIn = computerPlay();
-        let playerIn = getInput();
-        let res = playRound(playerIn, compIn);
-        switch(res) {
-            case 0:
-                console.log("Tie!");
-                break;
-            case 1:
-                playerScore++;
-                console.log('You win, ' + playerIn + ' beats ' + compIn + "!");
-                break;
-            default:
+            case 'paper':
                 compScore++;
-                console.log('You lose, ' + compIn + ' beats ' + playerIn + "!");
-        }        
+                console.log(compScore);
+
+                break;
+            case 'scissors':
+                playerScore++;
+                break;
+            default:
+        }
     }
-    if (playerScore > compScore) {
-        console.log('You Win!');
+    else if (playerSelection === 'paper') {
+        switch(computerSelection) {
+            case 'rock':
+                playerScore++;
+                break;
+            case 'scissors':
+                compScore++;
+                console.log(compScore);
+                break;
+            default:
+        }
     }
-    else if (compScore > playerScore) {
-        console.log('You lose!');
+    else if (playerSelection === 'scissors') {
+        switch(computerSelection) {
+            case 'rock':
+                compScore++;
+                console.log(compScore);
+                break;
+            case 'scissors':
+                playerScore++;
+                break;
+            default:
+        }
     }
-    else {
-        console.log('Tie!')
-    }
+    console.log(playerScore);
+    console.log(compScore);
+    displayRes(playerSelection, computerSelection);
 }
 
-game();
+
+
+
+const buttons = document.querySelectorAll('button')
+const pScore = document.querySelector('.pScore');
+const cScore = document.querySelector('.cScore');
+const chose = document.querySelector('.chose');
+const res = document.querySelector('.res');
+const rbtn = document.querySelector('#rbtn');
+const pbtn = document.querySelector('#pbtn');
+const sbtn = document.querySelector('#sbtn');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      playRound(button.textContent.toLowerCase());
+    });
+  });
+
+
